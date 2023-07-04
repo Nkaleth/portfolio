1c644dd5055ef0dac568d05652a6927871de05ac
Nkaleth <nseguralu@gmail.com>
2023-07-04 13:02:47 -0500

    ADD all the required schemas for back end

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,4 +1,19 @@
 import about from "./about"
+import brands from "./brands"
+import contact from "./contact"
+import experiences from "./experiences"
+import skills from "./skills"
 import testimonials from "./testimonials"
+import workExperience from "./workExperience"
+import works from "./works"
 
-export const schemaTypes = [testimonials, about]
+export const schemaTypes = [
+  testimonials,
+  about,
+  brands,
+  contact,
+  experiences,
+  skills,
+  workExperience,
+  works
+]
