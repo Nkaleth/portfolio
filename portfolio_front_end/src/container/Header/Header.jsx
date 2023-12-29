3f398615fa95cc37bf2280b73744c3eb8a6ea2d1
Nkaleth <nseguralu@gmail.com>
2023-12-28 20:58:14 -0500

    Update header circles main skills

--- portfolio_front_end/src/container/Header/Header.jsx
+++ portfolio_front_end/src/container/Header/Header.jsx
@@ -66,7 +66,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className='app__header-circles'
       >
-        {[images.javascript, images.react, images.ruby].map((circle, index) => (
+        {[images.javascript, images.react, images.vue].map((circle, index) => (
           <div className='circle-cmp app__flex' key={`circle-${index}`}>
             <img src={circle} alt="circle"/>
           </div>
